52af31a1ec8c81db42c15c895f434246554560fd
robbiebmorris <robbiebmorris@gmail.com>
2022-04-24 19:30:31 +0800

    6hrs

--- main.py
+++ main.py
@@ -1,52 +1,57 @@
-#Sodoku game!
-#create your own sodoku boards, generate them automatically, and play!
+# Sodoku game!
+# create your own sodoku boards, generate them automatically, and play!
 import csv
 import tkinter as tk
 
 
 class Sudoku:
 
-    #file_location:
-    #data: 2D array of tuples, with (given, correct) values
-    def __init__(self, file_location="none", data=0):
+    # file_location:
+    # data: 2D array of tuples, with (given, correct) values
+    def __init__(self, file_location="none", data=False):
         self.file_location = file_location
         self.data = data
-        
-        self.empty_board = self.create_empty_board(data)
+        if (self.data == False):
+            self.data = self.import_csv()
+
+        self.spaces = []
+        self.empty_board = self.create_empty_board(self.data)
         self.create_window()
 
-   
-    #could add a "create sodoku" function to create random games! 
-    # printing it to csv to save a nice board, wordle style  
+    # could add a "create sodoku" function to create random games!
+    # printing it to csv to save a nice board, wordle style
+
     def create_csv(self):
         file = open(self.file_location, 'w')
         csvwriter = csv.writer(file)
         for row in self.data:
             csvwriter.writerow(row)
 
-    #3bi
-    #imports a prewritten sudoku game from a csv file
+    # 3bi
+    # imports a prewritten sudoku game from a csv file
     def import_csv(self):
         file = open(self.file_location)
         csvreader = csv.reader(file)
-        
+
         arr = []
         for row in csvreader:
             try:
                 temp = []
                 for tup in row:
-                    temp.append(tuple(map(int, tup.replace('(', '').replace(')', '').split(', '))))
+                    temp.append(
+                        tuple(map(int, tup.replace('(', '').replace(')', '').split(', '))))
+                print(temp)
                 arr.append(temp)
             except:
-                print("Not a tuple: " + str(row))
+                print("Error with this part of the CSV: " + str(row))
                 arr.append(row)
         file.close()
-        self.data = arr
+        return arr
 
     def set_data(self, data):
         self.data = data
 
-    #input_arr
+    # input_arr
     def create_empty_board(self, input_arr):
         empty_board = []
         for i in range(len(input_arr)):
@@ -54,11 +59,11 @@ class Sudoku:
             for j in range(len(input_arr[i])):
                 temp.append(input_arr[i][j][0])
             empty_board.append(temp)
-        
+
         return empty_board
 
     def check_space(self, tup):
-        if (tup[0] == tup[1] or tup[1] == 'X'):
+        if (tup[0] == tup[1] or tup[1] == 0):
             return True
         return False
 
@@ -68,75 +73,84 @@ class Sudoku:
     def get_empty_board(self):
         return self.empty_board
 
+    # gui stuff!
+
     def create_window(self):
         window = tk.Tk()
 
+        # create the empty text boxes of grid
 
-#sodoku analytics!
+        for i in range(9):
+            temp = []
+            for j in range(9):
+                temp.append(tk.Text(window, height=1, width=2,
+                            font=("Arial", 25)))
+            self.spaces.append(temp)
 
-#arr: [(x, y), (x, y), (x, y)...]
-def analyze_one(arr):
-    val = 0
-    for tup in arr:
-        val += tup[0] * tup[1]
-    return val
+        # insert values into the text boxes based on data array
+        self.insert_values()
 
-#3c
-#arr: [[(x, y), (x, y)...], [(x, y), (x, y)...], ...]
-def get_weighted_scores(arr):
-    weighted_arr = [] 
-    for tup in arr:
-        if (type(tup[0][0]) != int):
-            continue
-        analyzed_tup = analyze_one(tup)
-        weighted_arr.append(analyzed_tup)
+        for row in range(len(self.spaces)):
+            for col in range(len(self.spaces[row])):
+                self.spaces[row][col].grid(row=row, column=col)
 
-    return weighted_arr
+        save_game = tk.Button(window, text="Save", command=self.create_csv())
+        check_game = tk.Button(window, text="Done", command=self.check_game())
+        exit_game = tk.Button(window, text="Exit", command=window.destroy)
 
-#weighted_scores: [x, y, z, ...]
-#direction: True or False
-# if true, list is from smallest to highest
-# if false, list is from highest to smallest
-def sort_scores(weighted_scores, direction):
-    arr = weighted_scores
-    for i in range(0, len(arr) - 1):
-        for j in range (0, len(arr) - 1 - i):
-            if (arr[j] > arr[j + 1]):
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
+        save_game.grid(row=9, column=3)
+        check_game.grid(row=9, column=4)
+        exit_game.grid(row=9, column=5)
 
-    if (direction):
-        return arr
-    elif (direction == False):
-        return arr[::-1]
-    else:
-        return -1
+        window.mainloop()
 
-def search(arr, target):
-    return 1
+    def insert_values(self):
 
-def main():  
-    
-    #'-' means empty space
-    #'X' means given uneditable number
-    data = [[('-',1), (9,'X'), (3,'X')],
-            [(1, 'X'), ('-',4), (4,'X')], 
-            [(6,'X'), (5,'X'), ('-',8)]]
-    
-    obj = Sudoku('data.csv', data)
-    
+        for row in range(len(self.spaces)):
+            for col in range(len(self.spaces[row])):
+                if (self.data[row][col][0] == 0):
+                    continue
+                self.spaces[row][col].insert(
+                    tk.END, str(self.data[row][col][0]))
+
+        # a2.insert(tk.END, "asdasd")
+
+    def get_space_input(self, row, col):
+        input = self.spaces[0][0].get("1.0", END)
+        return input
+
+    def check_game(self):
+        return 1
+
+
+def main():
+
+    # 0 in first part of tuple means empty space
+    # 0 in second part of tuple means given uneditable number
+    data = [[(0, 1), (9, 0), (3, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0)],
+            [(1, 0), (0, 4), (4, 0), (1, 0), (1, 0),
+             (1, 0), (1, 0), (1, 0), (1, 0)],
+            [(6, 0), (5, 0), (0, 8), (1, 0), (1, 0),
+             (1, 0), (1, 0), (1, 0), (1, 0)],
+            [(0, 1), (9, 0), (3, 0), (1, 0), (1, 0),
+             (1, 0), (1, 0), (1, 0), (1, 0)],
+            [(1, 0), (0, 4), (4, 0), (1, 0), (1, 0),
+             (1, 0), (1, 0), (1, 0), (1, 0)],
+            [(6, 0), (5, 0), (0, 8), (1, 0), (1, 0),
+             (1, 0), (1, 0), (1, 0), (1, 0)],
+            [(0, 1), (9, 0), (3, 0), (1, 0), (1, 0),
+             (1, 0), (1, 0), (1, 0), (1, 0)],
+            [(1, 0), (0, 4), (4, 0), (1, 0), (1, 0),
+             (1, 0), (1, 0), (1, 0), (1, 0)],
+            [(6, 0), (5, 0), (0, 8), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0)]]
+
+    obj = Sudoku('data.csv')
+
     obj.create_csv()
-    csv = obj.import_csv()
-    
+    # csv = obj.import_csv()
+
     print(obj.get_empty_board())
-    
-    #print(csv)
-    # scores = get_weighted_scores(csv)
-    # print(scores)
-    # x = sort_scores(scores, True)
-    # print(scores)
-    # print(x)
+
 
 if __name__ == "__main__":
     main()
