2e2433d6f274c99f4714ae1785d4df4db682427b
robbiebmorris <robbiebmorris@gmail.com>
2022-04-30 18:00:31 +0800

    fin

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 # Sodoku game!
-# create your own sodoku boards, generate them automatically, and play!
 import csv
 import tkinter as tk
 
-
 class Sudoku:
 
-    # file_location:
+    # file_location: String
     # data: 2D array of tuples, with (given, correct) values,
     def __init__(self, file_location="none", data=False):
 
@@ -21,6 +19,27 @@ class Sudoku:
 
         self.rules_window()
 
+    # imports a prewritten sudoku game from a csv file
+    def import_csv(self):
+        file = open(self.file_location)
+        csvreader = csv.reader(file)
+
+        data_arr = []
+        for row in csvreader:
+            #for reach row in csv file, convert the string into 2d array of tuples
+            try:
+                temp = []
+                for tup in row:
+                    temp.append(
+                        tuple(map(int, tup.replace('(', '').replace(')', '').split(', '))))
+                data_arr.append(temp)
+            #if converting to type 2d array of tuples throws error, print out to console what went wrong
+            except:
+                print("Error with this part of the CSV: " + str(row))
+                data_arr.append(row)
+        file.close()
+        return data_arr
+
     # export the sodoku game to keep for later!
     # standard documentation-given code for writing using csv module in Python: found online
     def export_csv(self):
@@ -29,25 +48,6 @@ class Sudoku:
         for row in self.data:
             csvwriter.writerow(row)
 
-    # 3bi: A program code segment which constains a list being used to manage complexity in the program
-    # imports a prewritten sudoku game from a csv file
-    def import_csv(self):
-        file = open(self.file_location)
-        csvreader = csv.reader(file)
-
-        arr = []
-        for row in csvreader:
-            try:
-                temp = []
-                for tup in row:
-                    temp.append(
-                        tuple(map(int, tup.replace('(', '').replace(')', '').split(', '))))
-                arr.append(temp)
-            except:
-                print("Error with this part of the CSV: " + str(row))
-                arr.append(row)
-        file.close()
-        return arr
 
     # little function to import a new game from csv and create a new game window to play it!
     def import_game(self):
@@ -58,6 +58,7 @@ class Sudoku:
     # input_arr: 2D array of tuples. e.x: [[(x, y), (x, y), (x, y)...], [(x, y), (x, y),...]...]
     def create_empty_board(self, input_arr):
         empty_board = []
+        #iterate over the input array and append first value of each tuple to a new 2d array
         for i in range(len(input_arr)):
             temp = []
             for j in range(len(input_arr[i])):
@@ -95,11 +96,14 @@ class Sudoku:
         # insert values into the text boxes based on data array
         self.insert_values()
 
+        #add a tag to every text box (space in the sudoku board)
+        #place the space in it's proper grid space
         for row in range(len(self.spaces)):
             for col in range(len(self.spaces[row])):
                 self.spaces[row][col].tag_add("tag", "1.0")
                 self.spaces[row][col].grid(row=row, column=col)
 
+        #declare all tkinter buttons
         save_game = tk.Button(self.window, text="Export",
                               command=self.export_csv)
         import_game = tk.Button(
@@ -109,35 +113,39 @@ class Sudoku:
         exit_game = tk.Button(self.window, text="Exit",
                               command=self.window.destroy)
 
-        save_game.grid(row=9, column=3)
-        import_game.grid(row=9, column=5)
-        check_game.grid(row=9, column=4)
-        exit_game.grid(row=10, column=4)
+        #put the buttons at the bottom of the GUI grid below the Sudoku board
+        save_game.grid(row=9, column=4, columnspan=5)
+        import_game.grid(row=9, columnspan=5)
+        check_game.grid(row=9, columnspan=9)
+        exit_game.grid(row=10, columnspan=9)
 
         self.window.mainloop()
 
     # shows the rules and introduces the game to the user!
     def rules_window(self):
+        #simple gui creation with tkinter
         self.window = tk.Tk()
-        self.window.geometry("550x290")
+        self.window.geometry("475x250")
 
         rules_text = tk.Text(self.window, height=14,
-                             width=200)
+                             width=150)
         rules_text.insert(
-            'end', "Welcome to Sodoku!\n\nFill in the empty boxes according to traditional Sodoku rules--rows, columns and 3x3 squares must contain unique numbers!\n\nA variety of buttons can be found below the board:\nDone: use this to check if your solution is correct!\nIf correct, the space will turn green!\nIf not, the space will turn red.\nInput: Use this to import new sodoku games from a CSV file!\nExport: Export the current sodoku game to a CSV file!\nExit: Leave the game :(\n\nClick Start to begin!")
+            'end', "Welcome to Sodoku!\n\nFill in the empty boxes according to traditional Sodoku rules--rows, columns and 3x3 squares must contain unique numbers!\n\nA variety of buttons can be found below the board:\nDone: use this to check if your solution is correct!\nIf correct, the space will turn green!\nIf not, the space will turn red.\nInport: Use this to import new sodoku games from a CSV file!\nExport: Export the current sodoku game to a CSV file!\nExit: Leave the game :(\n\nClick Start to begin!")
         rules_text.config(state='disabled')
         start_game = tk.Button(self.window, text="Start!",
                                command=self.create_game)
 
-        rules_text.pack(pady=10, padx=20)
-        start_game.pack(pady=2)
+        rules_text.pack(pady=10, padx=15)
+        start_game.pack()
 
         self.window.mainloop()
 
     # inserts values into the previously created text boxes based on data array
     def insert_values(self):
-        for row in range(len(self.spaces)):
-            for col in range(len(self.spaces[row])):
+        #iterate over 2d array self.data
+        for row in range(len(self.data)):
+            for col in range(len(self.data[row])):
+                #if the space is blank, continue. else, add the given values into the board and make them uneditable
                 if (self.data[row][col][0] == 0):
                     continue
                 self.spaces[row][col].insert(
@@ -152,42 +160,41 @@ class Sudoku:
         return self.spaces[row][col].get("1.0", "end")
 
     # evaluates a singular space to check if the user has inputted the currect value
-    # row: int between 0-8
-    # col: int between 0-8
-    def evaluate_space(self, row, col):
-        if (self.get_space_input(row, col) == "\n"):
-            self.errors += 1
-            return
+    # arr: 2d with tuples inside [[(x, y), (x, y)...], [(x, y)], []...]
+    # row: int between 0 and 8
+    def evaluate_row(self, arr, row):
+        for col in range(len(arr[row])):
+            if (self.get_space_input(row, col) == "\n"):
+                self.errors += 1
+                continue
 
-        # turns background of textbox gray if the answer is given
-        elif (self.data[row][col][1] == 0):
-            self.spaces[row][col].tag_config(
-                "tag", background="gray")
+            # turns background of textbox gray if the answer is given
+            elif (arr[row][col][1] == 0):
+                self.spaces[row][col].tag_config(
+                    "tag", background="gray")
 
-        # turns background green if answer is correct
-        elif (int(self.get_space_input(row, col)) == self.data[row][col][1]):
-            self.spaces[row][col].tag_config(
-                "tag", background="green")
-        # turns background red if answer is incorrect
-        else:
-            self.spaces[row][col].tag_config("tag", background="red")
-            self.errors += 1
+            # turns background green if answer is correct
+            elif (int(self.get_space_input(row, col)) == arr[row][col][1]):
+                self.spaces[row][col].tag_config(
+                    "tag", background="green")
+            # turns background red if answer is incorrect
+            else:
+                self.spaces[row][col].tag_config("tag", background="red")
+                self.errors += 1
 
     # iterates over the 2d array of the board and evaluates each space
     def check_game(self):
-        for row in range(len(self.spaces)):
-            for col in range(len(self.spaces[row])):
-                self.evaluate_space(row, col)
+        for row in range(len(self.data)):
+            self.evaluate_row(self.data, row)
 
         # creates a pop up window to indicate to the user how they did
-
         pop_up = tk.Toplevel(self.window)
         pop_up.geometry("250x150")
 
         # if they solved the whole board correctly, indicate they have won!
         if (self.errors == 0):
             mistakes = tk.Label(
-                pop_up, text="You completed the game with no mistakes!")
+                pop_up, text="You completed the game\n with no mistakes!")
             win = tk.Label(pop_up, text="You win!")
             mistakes.pack(pady=20)
             win.pack(pady=10)
@@ -206,24 +213,18 @@ def main():
 
     # 0 in first part of tuple means empty space
     # 0 in second part of tuple means given uneditable number
-    data = [[(0, 3), (5, 0), (2, 0), (0, 4), (0, 7), (6, 0), (0, 1), (0, 8), (0, 9)],
-            [(1, 0), (6, 0), (0, 8), (9, 0), (0, 5),
-             (0, 2), (0, 7), (0, 3), (4, 0)],
-            [(0, 7), (4, 0), (9, 0), (8, 0), (0, 1),
-             (3, 0), (6, 0), (2, 0), (0, 5)],
-            [(4, 0), (0, 2), (0, 5), (0, 6), (0, 9),
-             (0, 7), (8, 0), (0, 1), (0, 3)],
-            [(0, 6), (8, 0), (3, 0), (2, 0), (0, 4),
-             (1, 0), (5, 0), (9, 0), (0, 7)],
-            [(0, 9), (0, 7), (1, 0), (0, 5), (0, 3),
-             (0, 8), (0, 4), (0, 6), (2, 0)],
-            [(0, 8), (9, 0), (7, 0), (3, 0), (0, 6),
-             (5, 0), (2, 0), (4, 0), (0, 1)],
-            [(2, 0), (0, 1), (0, 4), (0, 7), (0, 8),
-             (9, 0), (0, 3), (5, 0), (6, 0)],
+    #default sudoku board data to be used in the construction of the Sudoku class
+    board_data = [[(0, 3), (5, 0), (2, 0), (0, 4), (0, 7), (6, 0), (0, 1), (0, 8), (0, 9)],
+            [(1, 0), (6, 0), (0, 8), (9, 0), (0, 5), (0, 2), (0, 7), (0, 3), (4, 0)],
+            [(0, 7), (4, 0), (9, 0), (8, 0), (0, 1), (3, 0), (6, 0), (2, 0), (0, 5)],
+            [(4, 0), (0, 2), (0, 5), (0, 6), (0, 9), (0, 7), (8, 0), (0, 1), (0, 3)],
+            [(0, 6), (8, 0), (3, 0), (2, 0), (0, 4), (1, 0), (5, 0), (9, 0), (0, 7)],
+            [(0, 9), (0, 7), (1, 0), (0, 5), (0, 3), (0, 8), (0, 4), (0, 6), (2, 0)],
+            [(0, 8), (9, 0), (7, 0), (3, 0), (0, 6), (5, 0), (2, 0), (4, 0), (0, 1)],
+            [(2, 0), (0, 1), (0, 4), (0, 7), (0, 8), (9, 0), (0, 3), (5, 0), (6, 0)],
             [(0, 5), (0, 3), (0, 6), (1, 0), (0, 2), (0, 4), (9, 0), (7, 0), (0, 8)]]
 
-    obj = Sudoku('data.csv')
+    obj = Sudoku('data.csv', board_data)
 
 
 if __name__ == "__main__":
